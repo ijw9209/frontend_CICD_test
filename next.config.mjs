187f077ba0025aea0e58c1c11fcdd279829ec6ea
ijw9209 <ijw9209@naver.com>
2024-07-02 01:51:08 +0900

    [DEL] config env 삭제

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    AWS_SSM_ACCESS_KEY: process.env.NEXT_PUBLIC_AWS_SSM_ACCESS_KEY,
-    AWS_SSM_SECRET_KEY: process.env.NEXT_PUBLIC_AWS_SSM_SECRET_KEY,
-    AWS_SSM_REGION: process.env.NEXT_PUBLIC_VOINOSIS_BASE_URL,
-    // PARTNER_URL: process.env.NEXT_PUBLIC_PARTNER_URL,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
