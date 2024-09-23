054309afa71d3494106d5679ec5892f81d233494
woong920901 <woong920901@gmail.com>
2024-09-23 15:04:19 +0900

    [ADD] 포트 및 ip 테스트 코드 추가

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { AuthRequestDto } from "@/dto/auth/auth-request.dto";
@@ -12,6 +12,9 @@ export default function Login() {
   const [error, setError] = useState("");
 
   const authRequestDto = new AuthRequestDto();
+
+  const [ip, setIp] = useState("");
+  const [port, setPort] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -44,9 +47,31 @@ export default function Login() {
     console.log(authRequestDto.errors);
   };
 
+  // 클라이언트 IP 주소 가져오기
+  useEffect(() => {
+    setPort(window.location.port);
+    const fetchIp = async () => {
+      try {
+        const response = await fetch("https://api64.ipify.org?format=json");
+        const data = await response.json();
+        setIp(data.ip); // IP 주소 설정
+      } catch (error) {
+        console.error("IP 주소를 가져오지 못했습니다.", error);
+      }
+    };
+
+    fetchIp();
+  }, []);
+
   return (
     <div>
       <h1>Login</h1>
+      {/* 포트 및 IP 정보 출력 */}
+      <div>
+        <p>현재 포트: {port || "포트 정보를 가져올 수 없습니다."}</p>
+        <p>현재 IP 주소: {ip || "IP 정보를 가져오는 중입니다..."}</p>
+      </div>
+
       <form onSubmit={handleSubmit}>
         <div>
           <label>Username:</label>
