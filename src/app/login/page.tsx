428da04413e1ca66cd26232eae33a33d8722c455
woong920901 <woong920901@gmail.com>
2024-09-24 09:55:30 +0900

    [CHG] 로그인 화면 변경 Login 2

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,9 +13,6 @@ export default function Login() {
 
   const authRequestDto = new AuthRequestDto();
 
-  const [ip, setIp] = useState("");
-  const [port, setPort] = useState("");
-
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -47,31 +44,9 @@ export default function Login() {
     console.log(authRequestDto.errors);
   };
 
-  // 클라이언트 IP 주소 가져오기
-  useEffect(() => {
-    setPort(window.location.port);
-    const fetchIp = async () => {
-      try {
-        const response = await fetch("https://api64.ipify.org?format=json");
-        const data = await response.json();
-        setIp(data.ip); // IP 주소 설정
-      } catch (error) {
-        console.error("IP 주소를 가져오지 못했습니다.", error);
-      }
-    };
-
-    fetchIp();
-  }, []);
-
   return (
     <div>
-      <h1>Login</h1>
-      {/* 포트 및 IP 정보 출력 */}
-      <div>
-        <p>현재 포트: {port || "포트 정보를 가져올 수 없습니다."}</p>
-        <p>현재 IP 주소: {ip || "IP 정보를 가져오는 중입니다..."}</p>
-      </div>
-
+      <h1>Login 2</h1>
       <form onSubmit={handleSubmit}>
         <div>
           <label>Username:</label>
