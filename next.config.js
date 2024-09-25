4c425ad3e084a6e03321b4072e6e7e39a8338ae0
woong920901 <woong920901@gmail.com>
2024-09-25 17:14:00 +0900

    [CHG] next.config 설정 제거 assetPrefix 제거

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const nextConfig = {
   // trailingSlash: true,
   swcMinify: true,
   reactStrictMode: true,
-  assetPrefix: ".",
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
