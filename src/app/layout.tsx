f21b7cbfacdb47b0653c6fe1c5df5dc96ac8203f
woong920901 <woong920901@gmail.com>
2024-09-25 17:04:03 +0900

    [CHG] next/font 주석처리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import AuthSessionProvider from "@/providers/auth-session-provider";
 // import "./globals.css";
 import AlertModal from "@/_components/modals/alert-modal/AlertModal";
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
